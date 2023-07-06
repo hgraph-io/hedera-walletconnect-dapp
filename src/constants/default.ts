1078c750b4786acdf9479fa99e637fd6bcad9ed1
Fenton Taylor <fentontaylor@gmail.com>
2023-07-06 16:22:15 -0600

    Update metadata to reference hedera

--- src/constants/default.ts
+++ src/constants/default.ts
@@ -1,3 +1,5 @@
+import { HEDERA_LOGO_URL } from "./logo";
+
 if (!process.env.NEXT_PUBLIC_PROJECT_ID)
   throw new Error("`NEXT_PUBLIC_PROJECT_ID` env variable is missing.");
 
@@ -21,10 +23,10 @@ export const DEFAULT_RELAY_URL = process.env.NEXT_PUBLIC_RELAY_URL;
 export const DEFAULT_LOGGER = "debug";
 
 export const DEFAULT_APP_METADATA = {
-  name: "React App",
-  description: "React App for WalletConnect",
-  url: "https://walletconnect.com/",
-  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  name: "Hedera dApp",
+  description: "Hedera dApp for WalletConnect",
+  url: "https://hedera.com/",
+  icons: [HEDERA_LOGO_URL],
 };
 
 /**
